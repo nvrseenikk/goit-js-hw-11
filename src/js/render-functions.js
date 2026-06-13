8c4073b5ef65943c1e4327d5b9f5b0b03f0c0aa1
dontcallmenik <dontcallmenik@gmail.com>
2026-06-13 22:33:08 +0200

    add image search app

---
+++ src/js/render-functions.js
@@ -0,0 +1,48 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      image => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${image.largeImageURL}">
+          <img
+            class="gallery-image"
+            src="${image.webformatURL}"
+            alt="${image.tags}"
+          />
+        </a>
+        <div class="info">
+          <p class="info-item"><b>Likes</b> ${image.likes}</p>
+          <p class="info-item"><b>Views</b> ${image.views}</p>
+          <p class="info-item"><b>Comments</b> ${image.comments}</p>
+          <p class="info-item"><b>Downloads</b> ${image.downloads}</p>
+        </div>
+      </li>
+    `
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+export function hideLoader() {
+  loader.classList.add('hidden');
+}
---
+++ src/main.js
@@ -0,0 +1,54 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const query = event.target.elements['search-text'].value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please enter a word for searching images.',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please, try again!',
+        });
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Error',
+        message: 'Something went wrong. Please try again.',
+      });
+      console.error(error);
+    })
+    .finally(() => {
+      hideLoader();
+      event.target.reset();
+    });
+});
